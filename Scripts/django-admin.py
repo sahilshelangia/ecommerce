08f71f6064dca2a1c862ed8d174541871a5c367c
sahilshelangia <sahilshelangia@gmail.com>
2020-04-05 22:38:18 +0530

    setup done

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sahil\OneDrive\Desktop\Development\ecommerce\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
